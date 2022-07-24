5088bb77ca02b7e14c882a3c40ab494b45c61050
BEKXCQ <fe494440@gmail.com>
2022-07-24 19:18:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['62857392175601'],
+  ['6285739217560'],
+  ['6283122197269', 'gempy', true]  
   
 ] 
 global.ownername = [['Gempy']]
